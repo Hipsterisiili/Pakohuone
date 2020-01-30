780421770f810679e69fc67cbc31468021a5b4d5
Histerisiili <rasmus.marttila@hotmail.com>
2020-01-30 14:42:19 +0200

    Muokattu Huone-luokkaa

--- Pakohuone/src/main/java/pakohuone/sovelluslogiikka/Huone.java
+++ Pakohuone/src/main/java/pakohuone/sovelluslogiikka/Huone.java
@@ -1,15 +1,45 @@
 package pakohuone.sovelluslogiikka;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rasmus
- */
 public class Huone {
+    private Ovi[] ovet;
+    private Avain[] avaimet;
+    private int avaintenMaara = 0;
+    private int ovienMaara = 0;
     
+    public Huone(){
+        ovet = new Ovi[100];
+        avaimet = new Avain[100];
+    }
+    /**
+     * Metodi lisää huoneeseen tiedon uudesta avaimesta.
+     *
+     * @param a Lisättävä avain
+     */
+    public void LisaaAvain(Avain a){
+        if(avaintenMaara >= 99){
+            return;
+        }
+        avaimet[avaintenMaara] = a;
+        avaintenMaara++;
+    }
+    /**
+     * Metodi lisää huoneeseen tiedon uudesta ovesta.
+     *
+     * @param a Lisättävä avain
+     */
+    public void LisaaOvi(Ovi o){
+        if(ovienMaara >= 99){
+            return;
+        }
+        ovet[ovienMaara] = o;
+        ovienMaara++;
+    }
+
+    public Ovi[] getOvet() {
+        return ovet;
+    }
+
+    public Avain[] getAvaimet() {
+        return avaimet;
+    }
 }
